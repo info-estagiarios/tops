494b7f5811ea718ac2da715a349a2c7c89b797ee
info-estagiarios <114098437+info-estagiarios@users.noreply.github.com>
2022-09-21 16:02:32 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+const fetchSvg = (image) => {
+    fetch(image.src)
+        .then((response) => response.text())
+        .then((response) => {
+            const span = document.createElement('span');
+            span.innerHTML = response;
+            const inlineSvg = span.getElementsByTagName('svg')[0];
+            image.parentNode.replaceChild(inlineSvg, image);
+            return true;
+        })
+};
+
+
+/*==== SCROLL ====*/
+
+window.sr = ScrollReveal({reset: true});
+
+sr.reveal('.container')
+sr.reveal('.distritos', {duration: 1500})
+sr.reveal('.container-msg', {duration: 1500})
