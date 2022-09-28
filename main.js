515fedaae0954a25f050aed754bdba3ecbe5d853
info-estagiarios <114098437+info-estagiarios@users.noreply.github.com>
2022-09-28 15:50:51 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const fetchSvg = (image) => {
+/*const fetchSvg = (image) => {
     fetch(image.src)
         .then((response) => response.text())
         .then((response) => {
@@ -8,7 +8,7 @@ const fetchSvg = (image) => {
             image.parentNode.replaceChild(inlineSvg, image);
             return true;
         })
-};
+};*/
 
 
 /*==== SCROLL ====*/
@@ -20,3 +20,26 @@ sr.reveal('.distritos', {duration: 1500})
 sr.reveal('.container-msg', {duration: 1500})
 sr.reveal('.top_1' , {duration: 1500})
 sr.reveal('.hamburger-menu' , {duration: 300})
+sr.reveal('#text-test' , {duration: 1400})
+
+/*====TEXT ANIMATION====*/
+
+var string = "Organizados";
+var array = string.split("");
+var timer;
+
+function frameLooper () {
+  if (array.length > 0) {
+    document.getElementById("territorio-span").innerHTML += array.shift();
+  } else {
+    clearTimeout(timer);
+      }
+  loopTimer = setTimeout('frameLooper()',70);
+}
+frameLooper();
+
+/*====BUTTON TO TOP====*/
+
+let topBtn = document.querySelector(".top-btn");
+topBtn.onclick = () => window.scrollTo({ top: 0, behavior: "smooth" });
+window.onscroll = () => window.scrollY > 500 ? topBtn.style.opacity = 1 : topBtn.style.opacity = 0;
