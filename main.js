76c775637c54f80afee47ee801cf8131fc461859
info-estagiarios <114098437+info-estagiarios@users.noreply.github.com>
2022-09-28 16:27:54 -0300

    Update main.js

--- main.js
+++ main.js
@@ -20,6 +20,7 @@ sr.reveal('.distritos', {duration: 1500})
 sr.reveal('.container-msg', {duration: 1500})
 sr.reveal('.top_1' , {duration: 1500})
 sr.reveal('#text-test' , {duration: 1400})
+sr.reveal('.hamburger-menu' , {duration: 200})
 
 /*====TEXT ANIMATION====*/
 
